be5cec8c0bb52b0a44c791b9d9f8f3b15d355315
Tech 4U <91035925+miltonTech4u@users.noreply.github.com>
2022-02-02 22:16:04 +0000

    New file.

---
+++ functions.py
@@ -0,0 +1,4 @@
+def a_function(): # defines a function called "a_function"
+  return "Hello World" # returns hello world when this function is called
+
+print(a_function()) # prints out the result of the function on the console
