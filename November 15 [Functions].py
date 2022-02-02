6c08edd2036893ce15955e8068442e50c0364f95
Tech 4U <91035925+miltonTech4u@users.noreply.github.com>
2022-02-02 22:20:31 +0000

    Changes

---
+++ November 15 [Functions].py
@@ -0,0 +1,10 @@
+def a_function(): # defines a function called "a_function"
+  return "Hello World" # returns hello world when this function is called
+
+def func_with_parameters(a, b): # contains two parameters: a and b. These parameters will                                      then be used in the function to do certain things
+  return a + b
+
+
+print(a_function()) # prints out the result of the function on the console
+
+print(func_with_parameters(5,6)) # prints out the sum of 5 and 6
--- functions.py
+++
@@ -1,4 +0,0 @@
-def a_function(): # defines a function called "a_function"
-  return "Hello World" # returns hello world when this function is called
-
-print(a_function()) # prints out the result of the function on the console
